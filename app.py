2141b5ea192d05af0ee000260bb1fd13a831da65
Digvijay Mahra <digvj92@gmail.com>
2025-10-22 01:42:09 +0530

    Added basic form and html templates

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! Your Week Planner is coming alive 🚀"
+    return render_template('plan_week.html')
+
+@app.route('/save-plan', methods=['POST'])
+def save_plan():
+    # We'll add database here next
+    print(request.form)  # For now, just print
+    return "Plan saved! (temporarily)"
 
 if __name__ == '__main__':
     app.run(debug=True)
